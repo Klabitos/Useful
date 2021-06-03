b9c95151df88fc902349047b0088aa3262d9038e
PabloCruz201120 <klabitos@usal.es>
2021-06-03 10:36:45 +0200

    add bd utils

--- utils/BDUtils.java
+++ utils/BDUtils.java
@@ -8,10 +8,10 @@ public class BDUtils {
 	public static Connection conexionBBDD(String nombreBase) throws Exception{
 		Class.forName("com.mysql.cj.jdbc.Driver"); //OBLIGATORIO Y SIEMPRE IGUAL
 		
-		String servidor = "jdbc:mysql://localhost:3306/"+nombreBase; //COGEMOS DE LA LOCAL LA QUE SE LLAMA CLASSIC MODELS
+		String servidor = ""+nombreBase; //COGEMOS DE LA LOCAL LA QUE SE LLAMA CLASSIC MODELS
 		String username = "root";
-		String password = "PracticaRoot"; //NUESTRA CONTRASEÑA DE AQUI
-		Connection conexionBD = DriverManager.getConnection(servidor, username, password); //ESTABLECEMOS CONEXION AL SERVIDOR CON EL USUARIO Y LA CONTRASEÑA
+		String password = ""; //NUESTRA CONTRASEï¿½A DE AQUI
+		Connection conexionBD = DriverManager.getConnection(servidor, username, password); //ESTABLECEMOS CONEXION AL SERVIDOR CON EL USUARIO Y LA CONTRASEï¿½A
 		
 		return conexionBD;
 	}
